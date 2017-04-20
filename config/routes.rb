586123c55280de59b535893ce76b992707322f75
janysev1 <janysev1@students.zhaw.ch>
2017-04-20 15:29:16 +0200

    Welcomepage and Controller added. Routes configured

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :media
   resources :tubecamstations
   resources :users
+
+  root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
