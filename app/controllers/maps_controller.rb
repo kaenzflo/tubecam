4727d94475a599e695ed8829722a1799ce95f7b8
janysev1 <janyseverin@gmx.net>
2017-04-22 16:33:10 +0200

    Plane map implementet

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,6 +2,17 @@ class MapsController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
 
   def index
+
+    path_to_file = File.join(Rails.root, 'app', 'assets', 'javascripts', 'maps', 'mapgeo.json')
+    map_geo_file = File.read(path_to_file)
+    @mapgeo = map_geo_file;
+
+    path_to_file = File.join(Rails.root, 'app', 'assets', 'javascripts', 'maps', 'mapstyle.json')
+    map_style_file = File.read(path_to_file)
+    @mapstyle = map_style_file;
+
+
+
     render template: "maps/map"
   end
 end
