8fc87ad0638682c4e26066f9ae67f21bacf4e956
janysev1 <janyseverin@gmx.net>
2017-05-09 22:04:42 +0200

    welcome page edited

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,16 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, :only => [:index]
 
   def index
+    @media = []
+    media = Medium.where(deleted: false)
+    for i in 1..6
+      @media << media[rand(0...media.size)]
+    end
+
+    @cloud_resource_image_url = 'https://' +
+        ENV['S3_HOST_NAME'] + '/' +
+        ENV['S3_BUCKET_NAME'] + '/'
+    @cloud_resource_thumbnail_url = @cloud_resource_image_url + ''
 
   end
 
