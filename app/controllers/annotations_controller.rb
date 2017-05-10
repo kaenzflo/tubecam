80946fae1f9f2521482d1d17838a524fb3b72d65
kaenzflo <kaenzflo@students.zhaw.ch>
2017-05-10 16:44:12 +0200

    Thumbs

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -5,39 +5,36 @@ class AnnotationsController < ApplicationController
     available_sequences = Sequence.where(deleted: false)
     sequences = available_sequences.where.not(id: Annotation.where(user_id: @user.id).select('sequence_id'))
     sequence = sequences[rand(0...sequences.size)]
-    media = Medium.where(sequence_id: sequence.id, deleted: false)
-    @medium = media.first
-    instantiate_vars(media)
-  end
-
-  def create
-    @medium_annotation = Annotation.new(annotations_params)
-    respond_to do |format|
-      if @medium_annotation.annotations_lookup_table_id != '' &&
-          @medium_annotation.user_id == current_user.id &&
-          @medium_annotation.save
-        format.html { redirect_to '/annotations/new', notice: 'Medium erfolgreich annotiert' }
-        format.json { render :'annotations/new', status: :created, location: @medium_annotation }
-      else
-        format.html { redirect_to '/annotations/new', alert: 'Annotation fehlgeschlagen' }
-        format.json { render json: @medium_annotation.errors, status: :unprocessable_entity }
-      end
-    end
+    sequence_media = Medium.where(sequence_id: sequence.id, deleted: false).order('frame ASC')
+    @medium = sequence_media.first
+    instantiate_vars(sequence_media)
   end
 
   def specific
     @user = current_user
-    media = Medium.all
-    available_media = media.where(deleted: false)
+    medium = Medium.find(specific_param[:id])
     own_annotations = Annotation.where(user_id: @user.id)
-    medium = available_media.find(specific_param[:id])
-    p '##############'
-    p medium.sequence_id.inspect
-    if own_annotations.where(sequence_id: medium.sequence_id)
+    if own_annotations.where(sequence_id: medium.sequence_id).nil?
       redirect_to '/annotations/new', alert: 'Medium bereits annotiert'
     else
-      @medium = available_media.find(specific_param[:id])
-      instantiate_vars(available_media)
+      @medium = medium
+      sequence_media = Medium.where(deleted: false, sequence_id: medium.sequence_id).order('frame ASC')
+      instantiate_vars(sequence_media)
+    end
+  end
+
+  def create
+    @annotation = Annotation.new(annotations_params)
+    respond_to do |format|
+      if @annotation.annotations_lookup_table_id != '' &&
+          @annotation.user_id == current_user.id &&
+          @annotation.save
+        format.html { redirect_to '/annotations/new', notice: 'Medium erfolgreich annotiert' }
+        format.json { render :'annotations/new', status: :created, location: @annotation }
+      else
+        format.html { redirect_to '/annotations/new', alert: 'Annotation fehlgeschlagen' }
+        format.json { render json: @annotation.errors, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -47,8 +44,8 @@ class AnnotationsController < ApplicationController
 
   private
 
-  def instantiate_vars(media)
-    @thumbnails = media.where(frame: @medium.frame..(@medium.frame + 3))
+  def instantiate_vars(sequence_media)
+    @thumbnails = sequence_media.where(frame: (@medium.frame + 1)..(@medium.frame + 4)) #.order('frame ASC')
     @cloud_resource_image_url = 'https://' +
         ENV['S3_HOST_NAME'] + '/' +
         ENV['S3_BUCKET_NAME'] + '/'
@@ -59,7 +56,7 @@ class AnnotationsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def annotations_params
-    params.require(:medium_annotation).permit(:user_id, :medium_id, :annotations_lookup_table_id)
+    params.require(:annotation).permit(:user_id, :sequence_id, :annotations_lookup_table_id)
   end
 
   def specific_param
