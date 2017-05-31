053c5caf3b2c0090d07419e3dbf5aac61dac9cfa
janysev1 <janyseverin@gmx.net>
2017-05-31 14:36:00 +0200

    Updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :tubecam_devices
-  resources :media
   resources :sequences
   resources :users
 
