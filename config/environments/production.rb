d93dbdc068c9b8cd388671e1c600d1ea6b4d0f0c
kaenzflo <kaenzflo@students.zhaw.ch>
2017-09-21 17:26:38 +0200

    Fix mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: 'www.tubecam.ch'}
 
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :ses
 
   config.action_mailer.smtp_settings = {
       user_name:      ENV['MAIL_USERNAME'],
